15fcbbb26536129358c5322d5e03d0411d54d7d2
Soumyadeep Roy <204379333+SoumyadeepRoy2006@users.noreply.github.com>
2026-06-03 11:50:42 +0530

    Added modules
    Hosting server, websocket server, other functions

---
+++ modules.js
@@ -0,0 +1,62 @@
+const express = require("express");
+module.exports = {
+   HostingServer: class {
+      exp = express();
+      wsocket = require("ws");
+      http = require("http").createServer(this.exp);
+      
+      constructor() {
+         this.exp.use(express.static("public"));
+         this.exp.get("/", (_, res) => {
+            res.render("index.ejs");
+         });
+         this.exp.get("/send", (_, res) => {
+            res.render("sender.ejs");
+         });
+         this.exp.get("/receive", (_, res) => {
+            res.render("receiver.ejs");
+         });
+      }
+
+      Start = (port=undefined) => {
+         if (port) {
+            this.http.listen(port, () => {
+               console.log(`Server running on port ${port}`);
+            });
+         } else {
+            this.http.listen(process.env.port, () => {
+               console.log(`Server live and running`);
+            });
+         }
+      }
+   },
+   TransferConnection: class {
+      sender = null;
+      receiver = null;
+      connectionID = null;
+
+      constructor (sender, connectionID) {
+         this.sender = sender;
+         this.connectionID = connectionID;
+      }
+
+      RemoveReceiver(reason=null) {
+         try {
+            if (reason) {
+               this.receiver.send(JSON.stringify({type: "RemovedFromConnection", reason: reason}));
+            } else {
+               this.receiver.send(JSON.stringify({type: "RemovedFromConnection"}));
+            }
+         } catch {}
+         this.receiver = null;
+      }
+   },
+   RandomID: (length=3) => {
+      var ID = '';
+      var characters = 'abcdefghijkmnpqrstuvwxyz123456789ABCDEFGHJKLMNPQRSTUVWXYZ';
+      for (let i = 0; i < length; i++) {
+         ID += characters.charAt(Math.floor(Math.random() * characters.length));
+      }
+      return ID;
+   }
+}
